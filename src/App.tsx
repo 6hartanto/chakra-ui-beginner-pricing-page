25dc63808a45eec6b9f286ddcc226099b1b124e2
6hartanto <6hartanto@gmail.com>
2022-05-13 13:24:17 +0700

    feat: add pricing comonent

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Header } from "./components/Header";
+import { Pricing } from "./components/Pricing";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Pricing />
     </div>
   );
 }
